888836fe9c7e1d9b49a99daee86aa08fbcd9c346
ouafa benamor <ouafa.benamor@gmail.com>
2022-10-20 20:38:06 +0100

    first commit

---
+++ client/src/Components/MovieCard/MovieCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+import Rating from "../Rating/Rating";
+
+import "./MovieCard.css";
+
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="movieCard">
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={movie.img} className="movieImg" />
+
+        <Card.Body className="cardText">
+          <Card.Title className="movieTitle">{movie.title} </Card.Title>
+          <div className="descprition">{movie.desc}</div>
+          {/* <span>{movie.rating}</span> */}
+          <Rating rating={movie.rating} />
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default MovieCard;
